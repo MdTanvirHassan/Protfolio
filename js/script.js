ca97c2661cb47da142402d567a694073fbd8233e
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-05-23 01:41:48 +0600

    add animated text, animated number counter.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*=======Scroll Reveal==== */
 ScrollReveal({
     reset: true,
     distance: '80px',
@@ -7,3 +8,12 @@ ScrollReveal({
 
 ScrollReveal().reveal('.heading-section', {origin: "top"});
 ScrollReveal().reveal('.about-img, .services-card, .project-card, .blog-card, .price-card', {origin: "bottom"});
+
+/* Typed JS */
+const typed = new Typed('.multi-text', {
+    strings: [ "A Web Designer", "A Developer", "I Love Design.", "I Love to Develop." ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
